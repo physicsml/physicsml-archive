29de69f5a161779a0a8ffe7cbaa1dd01115d37e3
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2016-08-05 11:32:35 -0400

    Site update

--- publish.py
+++ publish.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 
 os.system("make")
@@ -7,4 +9,4 @@ os.system("git add category/*.html")
 os.system("git add tag/*.html")
 os.system("git add develop/content/*")
 os.system("git commit -a -m \"Site update\"")
-os.system("git push")
+#os.system("git push")
