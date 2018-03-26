5a8df10c1faf3ce246899abd3cd4128ec675bf16
Anna Go <golubevanna@gmail.com>
2018-03-26 09:55:10 -0400

    Site update

--- develop/pelicanconf.py
+++ develop/pelicanconf.py
@@ -53,4 +53,4 @@ DISPLAY_PAGES_ON_MENU = False
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+#RELATIVE_URLS = True
